f9030914b2abbc6ef33cee9121013760b4a70d8a
Sunset <nilay.sabnis05@gmail.com>
2024-03-17 02:14:41 +0530

    updates

--- web/pages/ai.js
+++ web/pages/ai.js
@@ -1,10 +1,19 @@
-function genresponse(response){
+/* function genresponse(){
     var userprompt=document.getElementById("aiinput").value;
-    eel.askgemini(userprompt);
+    let response=eel.askgemini(userprompt);
 
     var parent = document.querySelector(".content");
     var resp=document.createElement("p");
     resp.textContent = response;
     console.log(response);
     parent.appendChild(resp);
+} */
+function genresponse() {
+    var question = document.getElementById("aiinput").value;
+    //document.getElementById("load").classList.add("loader");
+    eel.askgemini(question)(displayResponse);
+}
+
+function displayResponse(response) {
+    document.getElementById("response").innerText = response;
 }
